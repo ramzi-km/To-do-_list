6d8b011d55ea77cbb6e6e121149698c272153323
Ramzi K.M <ramzikm3@gmail.com>
2023-05-25 23:56:57 +0530

    add task completed

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Task } from '../../task';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
-  day!:string;
+  day!: string;
   reminder: boolean = false;
 
+  onSubmit() {
+    if (!this.text || !this.text.trim()) {
+      alert('please add a task!');
+      return;
+    }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,29 +1,32 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Tasks } from '../mock-tasks';
 import { Task } from '../task';
 
 const httpOptions = {
-  headers:new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
-}
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks';
-  constructor(private http:HttpClient) { }
-  getTasks(): Observable<Task[]>{
-    return this.http.get<Task[]>(this.apiUrl)
+  constructor(private http: HttpClient) {}
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
   }
-  deleteTask(task:Task): Observable<Task> {
-    const url = `${this.apiUrl}/${task.id}`
-    return this.http.delete<Task>(url)
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
-  updateTaskReminder(task:Task): Observable<Task> {
-    const url = `${this.apiUrl}/${task.id}`
-    return this.http.put<Task>(url, task, httpOptions)
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 }
